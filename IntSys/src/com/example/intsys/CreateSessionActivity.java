60d2a576d97a5ca123579b59df4de32468508dc6
Manuel-Ko <fergo34@yahoo.de>
2014-06-20 23:41:44 +0200

    Restructured repo to handle betterPcikers Library added a date picker for creating a session

---
+++ IntSys/src/com/example/intsys/CreateSessionActivity.java
@@ -0,0 +1,129 @@
+package com.example.intsys;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Build;
+
+import com.doomonafireball.betterpickers.calendardatepicker.CalendarDatePickerDialog;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+
+
+public class CreateSessionActivity extends FragmentActivity{
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_session);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.create_session, menu);
+		return true;
+	}
+	
+    @Override
+    protected void onStop() {
+    	super.onStop();
+    	this.finish();
+    }
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment
+		implements CalendarDatePickerDialog.OnDateSetListener{
+		
+		private FragmentActivity fragmentContext;
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public void onAttach(Activity activity) {
+		    fragmentContext=(FragmentActivity) activity;
+		    super.onAttach(activity);
+		}
+		
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_create_session,
+					container, false);
+			
+            rootView.findViewById(R.id.confirmCreateSessionButton).setOnClickListener(
+				new View.OnClickListener() {
+	        		@Override
+	        		public void onClick(View view) {
+            			Intent intent = new Intent(getActivity(), SessionActivity.class);
+            			getActivity().startActivity(intent);
+	        		}
+	    		}							
+			);
+            
+            rootView.findViewById(R.id.datePickerButon).setOnClickListener(
+    				new View.OnClickListener() {
+    	        		@Override
+    	        		public void onClick(View view) {
+    	        			FragmentManager fm = fragmentContext.getSupportFragmentManager();
+    	                    CalendarDatePickerDialog calendarDatePickerDialog = CalendarDatePickerDialog
+    	                            .newInstance(PlaceholderFragment.this, 2014, 1,
+    	                                    1);
+    	                    calendarDatePickerDialog.show(fm,"someText");    	                    
+    	        		}
+    	    		}							
+    			);
+			
+			return rootView;
+		}
+
+		@Override
+		public void onDateSet(CalendarDatePickerDialog dialog, int year,
+				int monthOfYear, int dayOfMonth) {
+			// TODO Auto-generated method stub
+			Calendar test = Calendar.getInstance();
+            test.set(year, monthOfYear, dayOfMonth);
+            SimpleDateFormat date = new SimpleDateFormat("EE d/MM/yy"); 
+            
+            Button sessionDate = (Button) fragmentContext.findViewById(R.id.datePickerButon);
+            sessionDate.setText( date.format(test.getTime()) );
+		}
+	}
+
+}
