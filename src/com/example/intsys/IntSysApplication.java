0ed18423ab229d30040f0bd92930f7053bf52b01
NoNoid <NoNoid@gmx-topmail.de>
2014-06-21 16:41:31 +0200

    extended Android Application class

---
+++ src/com/example/intsys/IntSysApplication.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.example.intsys;
+
+import com.example.intsys.data.DataSingleton;
+
+import android.app.Application;
+
+/**
+ * @author NoNoid
+ *
+ */
+public class IntSysApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+
+}
